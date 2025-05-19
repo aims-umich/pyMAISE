eaf1676a00819f56870711ee6774ec0cd4461fa0
Nataly Panczyk <npanczyk@umich.edu>
2025-05-19 16:13:40 -0400

    fixes html and notebook

--- pyMAISE/__init__.py
+++ pyMAISE/__init__.py
@@ -16,7 +16,7 @@ from pyMAISE.settings import ProblemType, init
 from pyMAISE.tuner import Tuner
 from pyMAISE.utils import Boolean, Choice, Fixed, Float, Int, _try_clear
 from pyMAISE.explain import _explain as explain
-from pyMAISE.explain import ShapExplainers
+from pyMAISE.explain._explain import ShapExplainers
 
 _try_clear()
 
